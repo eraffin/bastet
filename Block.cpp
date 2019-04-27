c0fdc0ad68c71ea7ea907a3b19b96d2b49fd58c6
eraffin <erwan.raffin@gmail.com>
2019-04-27 14:16:35 +0200

    proto 2: all blocks are weird

--- Block.cpp
+++ Block.cpp
@@ -24,101 +24,108 @@ namespace Bastet{
 
   BlockArray blocks={{
       BlockImpl(COLOR_PAIR(7),(OrientationMatrix){{ //O //should be yellow, but I find no portable way to output a yellow solid block character in ncurses.
-	    {{//orientation 0 (initial
+	    // 0 O - 1 I - 2 Z - 3 T
+	    {{//orientation 0 O (initial
 		{1,1},{2,1},{1,0},{2,0}
 	      }},
-	      {{//orientation 1
-		  {1,1},{2,1},{1,0},{2,0}
-		}},
-		{{//orientation 2
-		    {1,1},{2,1},{1,0},{2,0}
-		  }},
-		  {{//orientation 3
-		      {1,1},{2,1},{1,0},{2,0}
-		    }}
-	  }}),
-      BlockImpl(COLOR_PAIR(4),(OrientationMatrix){{ //I
-	    {{//orientation 0 (initial)
-		{0,1},{1,1},{2,1},{3,1}
-	      }},
-	      {{//orientation 1
+	      {{//orientation 1 I
 		  {2,3},{2,1},{2,2},{2,0}
 		}},
-		{{//orientation 2
-		    {0,2},{1,2},{2,2},{3,2}
-		  }},
-		  {{//orientation 3
-		      {1,3},{1,1},{1,2},{1,0}
-		    }}
-	  }}),
-      BlockImpl(COLOR_PAIR(1),(OrientationMatrix){{ //Z
-	    {{//orientation 0 (initial
-		{1,1},{2,1},{0,0},{1,0}
-	      }},
-	      {{//orientation 1
-		  {1,2},{1,1},{2,1},{2,0}
-		}},
-		{{//orientation 2
+		{{//orientation 2 Z
 		    {1,2},{2,2},{0,1},{1,1}
 		  }},
-		  {{//orientation 3
-		      {0,2},{0,1},{1,1},{1,0}
-		    }}
-	  }}),
-      BlockImpl(COLOR_PAIR(5),(OrientationMatrix){{ //T
-	    {{//orientation 0 (initial
-		{0,1},{1,1},{2,1},{1,0}
-	      }},
-	      {{//orientation 1
-		  {1,2},{1,1},{2,1},{1,0}
-		}},
-		{{//orientation 2
-		    {1,2},{0,1},{1,1},{2,1}
-		  }},
-		  {{//orientation 3
+		  {{//orientation 3 T
 		      {1,2},{0,1},{1,1},{1,0}
 		    }}
 	  }}),
-      BlockImpl(COLOR_PAIR(6),(OrientationMatrix){{ //J
-	    {{//orientation 0 (initial
+      BlockImpl(COLOR_PAIR(4),(OrientationMatrix){{ //I
+	    // 0 J - 1 S - 2 L - 3 O
+	    {{//orientation 0 J (initial
 		{0,1},{1,1},{2,1},{0,0}
 	      }},
-	      {{//orientation 1
-		  {1,2},{1,1},{1,0},{2,0}
+	      {{//orientation 1 S
+		  {2,2},{1,1},{2,1},{1,0}
 		}},
-		{{//orientation 2
-		    {2,2},{0,1},{1,1},{2,1}
+		{{//orientation 2 L
+		    {0,2},{0,1},{1,1},{2,1}
 		  }},
-		  {{//orientation 3
+		  {{//orientation 3 O
+		      {1,1},{2,1},{1,0},{2,0}
+		    }}
+	  }}),
+      BlockImpl(COLOR_PAIR(1),(OrientationMatrix){{ //Z
+	    // 0 I - 1 Z - 2 T - 3 J
+	    {{//orientation 0 I (initial)
+		{0,1},{1,1},{2,1},{3,1}
+	      }},
+	      {{//orientation 1 Z
+		  {1,2},{1,1},{2,1},{2,0}
+		}},
+		{{//orientation 2 T
+		    {1,2},{0,1},{1,1},{2,1}
+		  }},
+		  {{//orientation 3 J
 		      {0,2},{1,2},{1,1},{1,0}
 		    }}
 	  }}),
-      BlockImpl(COLOR_PAIR(3),(OrientationMatrix){{ //S
+      BlockImpl(COLOR_PAIR(5),(OrientationMatrix){{ //T
+	    // 0 S - 1 L - 2 O - 3 I
 	    {{//orientation 0 (initial
 		{0,1},{1,1},{1,0},{2,0}
 	      }},
-	      {{//orientation 1
-		  {2,2},{1,1},{2,1},{1,0}
+	      {{//orientation 1 L
+		  {1,2},{2,2},{1,1},{1,0}
 		}},
-		{{//orientation 2
-		    {0,2},{1,2},{1,1},{2,1}
+		{{//orientation 2 O
+		    {1,1},{2,1},{1,0},{2,0}
 		  }},
-		  {{//orientation 3
+		  {{//orientation 3 I
+		      {1,3},{1,1},{1,2},{1,0}
+		    }}
+	  }}),
+      BlockImpl(COLOR_PAIR(6),(OrientationMatrix){{ //J
+	    // 0 Z - 1 T - 2 J - 3 S
+	    {{//orientation 0 Z (initial
+		{1,1},{2,1},{0,0},{1,0}
+	      }},
+	      {{//orientation 1 T
+		  {1,2},{1,1},{2,1},{1,0}
+		}},
+		{{//orientation 2 J
+		    {2,2},{0,1},{1,1},{2,1}
+		  }},
+		  {{//orientation 3 S
 		      {1,2},{0,1},{1,1},{0,0}
 		    }}
 	  }}),
-      BlockImpl(COLOR_PAIR(2),(OrientationMatrix){{ //L
-	    {{//orientation 0 (initial
+      BlockImpl(COLOR_PAIR(3),(OrientationMatrix){{ //S
+	    // 0 L - 1 O - 2 I - 3 Z
+	    {{//orientation 0 L (initial
 		{0,1},{1,1},{2,1},{2,0}
 	      }},
-	      {{//orientation 1
-		  {1,2},{2,2},{1,1},{1,0}
+	      {{//orientation 1 O
+		  {1,1},{2,1},{1,0},{2,0}
 		}},
-		{{//orientation 2
-		    {0,2},{0,1},{1,1},{2,1}
+		{{//orientation 2 I
+		    {0,2},{1,2},{2,2},{3,2}
 		  }},
-		  {{//orientation 3
-	  {1,2},{1,1},{0,0},{1,0}
+		  {{//orientation 3 Z
+		      {0,2},{0,1},{1,1},{1,0}
+		    }}
+	  }}),
+      BlockImpl(COLOR_PAIR(2),(OrientationMatrix){{ //L
+	    // 0 T - 1 J - 2 S - 3 L 
+	    {{//orientation 0 T (initial
+		{0,1},{1,1},{2,1},{1,0}
+	      }},
+	      {{//orientation 1 J
+		  {1,2},{1,1},{1,0},{2,0}
+		}},
+		{{//orientation 2 S
+		    {0,2},{1,2},{1,1},{2,1}
+		  }},
+		  {{//orientation 3 L
+		      {1,2},{1,1},{0,0},{1,0}
 		    }}
 	  }})
     }};
